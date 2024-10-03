1f85f1d3473c9e750f88c0a0731c32e7aed94e3f
Nicolas Steven Gutierrez Castiyejo <61254982+imnicoo7@users.noreply.github.com>
2024-10-03 14:31:59 -0500

    Update dataframe.py

--- dataframe.py
+++ dataframe.py
@@ -1,13 +1,5 @@
-import pandas as pd
-
 def generar_dataframe():
-    # data frame generado
-    data = {'Tarea': ['Hijo 1', 'Hijo 2'],
-            'Estado': ['Pendiente', 'Completado']}
-    df = pd.DataFrame(data)
-
-    # Guardar el DataFrame en un archivo CSV para que lo use Jenkins
-    df.to_csv('resultado.csv', index=False)
-
+    print('Hola prueba desde Jenkins')
+    print ('12+8')
 if __name__ == "__main__":
     generar_dataframe()
